9e25719f74c96aac849c55e182a338f145cd07d9
JunaidKhan19 <khanjunaid1719@gmail.com>
2024-08-15 16:24:02 +0530

    connected index.js and connectiondb.js to configure any file connection issue

---
+++ src/db/connectiondb.js
@@ -0,0 +1,5 @@
+function printMessage() {
+    console.log("Connection to the database established successfully!");
+}
+
+export { printMessage }; // ES Module syntax for exporting the function
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import { printMessage } from './db/connectiondb.js'; // Import the function from connectiondb.js
+
+printMessage(); // Call the function to print the message
 console.log("hii");
