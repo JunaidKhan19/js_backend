2d1e7ca24fdc7180190634a28bde741174e21d91
JunaidKhan19 <khanjunaid1719@gmail.com>
2024-08-17 00:51:13 +0530

    connected db at mongodb atlas using approach 1 and used cors , cookie parser in app.js to provide some configuration

--- src/db/connectiondb.js
+++
@@ -1,5 +0,0 @@
-function printMessage() {
-    console.log("Connection to the database established successfully!");
-}
-
-export { printMessage }; // ES Module syntax for exporting the function
--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-import { printMessage } from './db/connectiondb.js'; // Import the function from connectiondb.js
+//import dotenv from 'dotenv';
+import mongoose from "mongoose";
+//import { printMessage } from './db/connectiondb.js';
+import { DB_NAME } from "./constants.js";
+import express from "express";
 
-printMessage(); // Call the function to print the message
-console.log("hii");
+/*
+// Debugging to check if dotenv is loaded
+console.log('Loading dotenv...');
+dotenv.config({ path: './.env' });
+console.log('dotenv loaded');
+*/
+
+// mongodb_uri and PORT are the environment variables and must be written in .env file
+//but i have written it here bcauz the .env was not loading in the index.js
+const app = express();
+const mongodb_uri = 'mongodb+srv://khanjunaid1719:Junaid123@cluster0.prf9s.mongodb.net/';
+const PORT = 3000;
+( async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${mongodb_uri}/${DB_NAME}`)
+        console.log(`\n DB host: ${connectionInstance.connection.host}`)
+        app.on("error" , (error) => {
+            console.log("ERROR: " , error);
+            throw error
+        })
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`)
+        })
+    } catch (error) {
+        console.log("ERROR: " , error)
+        throw error
+    }
+})()
