b6c1e1a2d4f5f3ae92d8513bd3011e4419aa9890
JunaidKhan19 <khanjunaid1719@gmail.com>
2024-08-18 20:45:13 +0530

    wrote some utils to handle async functions , apierrors, apiresponse

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,22 @@
+//approach 1 using promises
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+               .catch((error) => next(error))
+    }
+}
+
+
+/*
+//approach 2 using try catch
+const asyncHandler = (requestHandler) => async(req, res, next) => {
+    try {
+        await requestHandler(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message:err.message
+        })
+    }
+}
+*/
